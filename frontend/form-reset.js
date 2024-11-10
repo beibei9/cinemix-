162a04a6d9bc0ec4c154aed493fed391426f927f
beibei9 <nygymettolaa4@gmail.com>
2024-11-10 16:55:22 +0600

    Delete frontend directory

--- frontend/form-reset.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('reset-btn').addEventListener('click', function() {
-    // Select all input fields within the form and reset their values
-    document.querySelectorAll('#subscription-form input, #subscription-form select, #subscription-form textarea').forEach(input => {
-        input.value = '';
-    });
-    document.getElementById('genre').selectedIndex = 0; // Set dropdown to the default option
-});
